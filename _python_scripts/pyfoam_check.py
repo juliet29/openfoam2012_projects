3f86b53a1e6ea13df4279f398960dfbc981ec3b9
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2022-08-29 10:04:06 -0700

    082922 start

--- _python_scripts/pyfoam_check.py
+++ _python_scripts/pyfoam_check.py
@@ -1,1 +1,10 @@
 from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
+ 
+f=ParsedParameterFile("pitzDaily/0/U")
+ 
+for b in f["boundaryField"]:
+    if "Wall" in b:
+        f["boundaryField"][b]["value"]="uniform (10 0 0)"
+        f["boundaryField"][b]["type"]="fixedValue"
+ 
+f.writeFile()
